eacf81b699f03e147ca0359b0142f8ff81933f43
Utemov Alexey <Ale22Xey@gmail.com>
2025-11-09 21:54:31 +0500

    fix const

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -8,7 +8,7 @@ func main() {
 		USD_TO_RUB = 75.50
 	)
 
-	EUR_TO_RUB := USD_TO_RUB / USD_TO_EUR
+	const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
 	fmt.Printf("Курсы конвертации:\n")
 	fmt.Printf("USD to EUR: %.4f\n", USD_TO_EUR)
